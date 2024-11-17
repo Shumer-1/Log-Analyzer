f5cac40bfe9071fcd22bcafab543d521273c951c
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-17 13:41:23 +0300

    feat(main): add main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,16 @@
 package backend.academy;
 
+import backend.academy.session.SessionPreparer;
+import java.io.PrintStream;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        // https://pastebin.com/raw/wnHK5UDf
+        // /home/vladislav/IdeaProjects/backend_academy/project3/backend_academy_2024_project_3-java-Shumer-1/**/logs.txt
+        PrintStream consolePrintStream = new PrintStream(System.out);
+        SessionPreparer sessionPreparer = new SessionPreparer(consolePrintStream);
+        sessionPreparer.sessionPrepare(args);
     }
 }
