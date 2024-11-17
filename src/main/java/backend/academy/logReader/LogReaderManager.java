8e6a23da5cf50d01b2922183df606face6c5487d
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-17 17:27:25 +0300

    refactor(logReader): make class utility

--- src/main/java/backend/academy/logReader/LogReaderManager.java
+++ src/main/java/backend/academy/logReader/LogReaderManager.java
@@ -3,7 +3,9 @@ package backend.academy.logReader;
 import backend.academy.exceptions.SetUpReaderException;
 import java.io.IOException;
 import java.util.regex.Pattern;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class LogReaderManager {
     private static final String URL_REGEX = "^https://[^/]+(/[^?]*)?";
 
