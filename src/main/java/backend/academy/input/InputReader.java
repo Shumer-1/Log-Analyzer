e5d84aa1d7c278cf702b7e10cadaf291255ba3bb
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-17 17:34:34 +0300

    refactor(input): add suppress warning for Cyclomatic Complexity

--- src/main/java/backend/academy/input/InputReader.java
+++ src/main/java/backend/academy/input/InputReader.java
@@ -6,9 +6,12 @@ import backend.academy.log.Field;
 import backend.academy.writer.OutputFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class InputReader {
 
+    @SuppressWarnings("CyclomaticComplexity")
     public static Config parse(String[] args) throws ArgumentsParseException {
         if (args.length == 0) {
             throw new ArgumentsParseException("Invalid value: nothing entered");
@@ -26,40 +29,49 @@ public class InputReader {
         while (i < args.length) {
             if (args[i].startsWith("--") && i + 1 == args.length) {
                 throw new ArgumentsParseException("Invalid value: nothing after --{statement} ");
-            } else if ("--filter-field".equals(args[i])) {
-                try {
-                    field = Field.valueOf(args[i + 1].toUpperCase());
-                } catch (IllegalArgumentException e) {
-                    throw new ArgumentsParseException("Invalid value after --filter-field");
-                }
-            } else if ("--path".equals(args[i])) {
-                path = args[i + 1];
-                flagPath = true;
-            } else if ("--filter-value".equals(args[i])) {
-                value = args[i + 1];
-            } else if ("--from".equals(args[i]) && i + 1 < args.length) {
-                try {
-                    from = LocalDate.parse(args[i + 1]);
-                } catch (DateTimeParseException e) {
-                    throw new ArgumentsParseException("Invalid value after --from");
-                }
-            } else if ("--to".equals(args[i])) {
-                try {
-                    to = LocalDate.parse(args[i + 1]);
-                } catch (DateTimeParseException e) {
-                    throw new ArgumentsParseException("Invalid value after --to");
-                }
-            } else if ("--format".equals(args[i])) {
-                try {
-                    outputFormat = OutputFormat.valueOf(args[i + 1].toUpperCase());
-                } catch (IllegalArgumentException e) {
-                    throw new ArgumentsParseException("Invalid value after --format");
-                }
-            } else {
-                throw new ArgumentsParseException("Invalid value: value without config (--{statement})");
+            }
+            switch (args[i]) {
+                case "--filter-field":
+                    try {
+                        field = Field.valueOf(args[i + 1].toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        throw new ArgumentsParseException("Invalid value after --filter-field");
+                    }
+                    break;
+                case "--path":
+                    path = args[i + 1];
+                    flagPath = true;
+                    break;
+                case "--filter-value":
+                    value = args[i + 1];
+                    break;
+                case "--from":
+                    try {
+                        from = LocalDate.parse(args[i + 1]);
+                    } catch (DateTimeParseException e) {
+                        throw new ArgumentsParseException("Invalid value after --from");
+                    }
+                    break;
+                case "--to":
+                    try {
+                        to = LocalDate.parse(args[i + 1]);
+                    } catch (DateTimeParseException e) {
+                        throw new ArgumentsParseException("Invalid value after --to");
+                    }
+                    break;
+                case "--format":
+                    try {
+                        outputFormat = OutputFormat.valueOf(args[i + 1].toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        throw new ArgumentsParseException("Invalid value after --format");
+                    }
+                    break;
+                default:
+                    throw new ArgumentsParseException("Invalid value: value without config (--{statement})");
             }
             i += 2;
         }
+
 
         if (!flagPath) {
             throw new ArgumentsParseException("Invalid value: path not specified");
