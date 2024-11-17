7f9e31f6e827d1f7b3ad128b7bf34373e2aa7ef2
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-17 13:37:08 +0300

    feat(statistics): add class storing statistics

---
+++ src/main/java/backend/academy/statistics/Statistics.java
@@ -0,0 +1,18 @@
+package backend.academy.statistics;
+
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public record Statistics(List<Path> filenames,
+                         LocalDate startDate,
+                         LocalDate endDate,
+                         int totalRequests,
+                         double averageResponseSize,
+                         long percentile95ResponseSize,
+                         List<Map.Entry<String, Long>> mostPopularResources,
+                         List<Map.Entry<Integer, Long>> mostPopularResponseCodes,
+                         List<Map.Entry<String, Long>> mostPopularMethods,
+                         List<Map.Entry<String, Long>> mostActiveUsers) {
+}
