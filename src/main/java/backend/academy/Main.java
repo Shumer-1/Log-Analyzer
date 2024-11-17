bbebf59a4060ddec6926a964d117ee7e600dd8c9
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-17 17:28:04 +0300

    refactor(main): delete comments

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -7,8 +7,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
-        // https://pastebin.com/raw/wnHK5UDf
-        // /home/vladislav/IdeaProjects/backend_academy/project3/backend_academy_2024_project_3-java-Shumer-1/**/logs.txt
         PrintStream consolePrintStream = new PrintStream(System.out);
         SessionPreparer sessionPreparer = new SessionPreparer(consolePrintStream);
         sessionPreparer.sessionPrepare(args);
