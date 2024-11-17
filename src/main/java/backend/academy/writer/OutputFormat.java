e22ddba1e6db1bf12d73f0bd53dd81abf563d345
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-17 17:31:50 +0300

    refactor(writer): refactor for checkstyle

--- src/main/java/backend/academy/writer/OutputFormat.java
+++ src/main/java/backend/academy/writer/OutputFormat.java
@@ -3,15 +3,17 @@ package backend.academy.writer;
 import lombok.Getter;
 
 @Getter public enum OutputFormat {
-    MARKDOWN("#","|------|------|\n", "", " |"),
-    ADOC("=", "|===\n", "|===\n", "");
 
+    MARKDOWN("#", "|------|------|\n", "", " |"),
+    ADOC("=", ADOC_TABLE_SYMBOL(), ADOC_TABLE_SYMBOL(), "");
+
+    @Getter private static final String ADOC_TABLE_SYMBOL = "|===\n";
     private final String header;
     private final String startTable;
     private final String endTable;
     private final String endColumn;
 
-    OutputFormat(String header, String startTable, String endTable, String endColumn){
+    OutputFormat(String header, String startTable, String endTable, String endColumn) {
         this.header = header;
         this.startTable = startTable;
         this.endTable = endTable;
