4dd6ef74ffebba3e56ca33bc295304d19cffe17f
Shumer-1 <borisov1908vlad@mail.ru>
2024-11-23 13:58:24 +0300

    refactor(input): modify class to apply Chain pattern

--- src/main/java/backend/academy/input/InputReader.java
+++ src/main/java/backend/academy/input/InputReader.java
@@ -2,10 +2,6 @@ package backend.academy.input;
 
 import backend.academy.config.Config;
 import backend.academy.exceptions.ArgumentsParseException;
-import backend.academy.log.Field;
-import backend.academy.writer.OutputFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -17,71 +13,42 @@ public class InputReader {
             throw new ArgumentsParseException("Invalid value: nothing entered");
         }
 
-        String path = "";
-        LocalDate from = LocalDate.MIN;
-        LocalDate to = LocalDate.MAX;
-        OutputFormat outputFormat = OutputFormat.MARKDOWN;
-        Field field = null;
-        String value = "";
+        Settings settings = new Settings();
+        ArgumentHandler pathHandler = new PathHandler();
+        ArgumentHandler filterFieldHandler = new FilterFieldHandler();
+        ArgumentHandler filterValueHandler = new FilterValueHandler();
+        ArgumentHandler formatHandler = new FormatHandler();
+        ArgumentHandler toTimeHandler = new ToTimeHandler();
+        ArgumentHandler fromTimeHandler = new FromTimeHandler();
 
-        boolean flagPath = false;
+        pathHandler.setNext(filterFieldHandler);
+        filterFieldHandler.setNext(filterValueHandler);
+        filterValueHandler.setNext(formatHandler);
+        formatHandler.setNext(toTimeHandler);
+        toTimeHandler.setNext(fromTimeHandler);
+
         int i = 0;
         while (i < args.length) {
             if (args[i].startsWith("--") && i + 1 == args.length) {
                 throw new ArgumentsParseException("Invalid value: nothing after --{statement} ");
             }
-            switch (args[i]) {
-                case "--filter-field":
-                    try {
-                        field = Field.valueOf(args[i + 1].toUpperCase());
-                    } catch (IllegalArgumentException e) {
-                        throw new ArgumentsParseException("Invalid value after --filter-field");
-                    }
-                    break;
-                case "--path":
-                    path = args[i + 1];
-                    flagPath = true;
-                    break;
-                case "--filter-value":
-                    value = args[i + 1];
-                    break;
-                case "--from":
-                    try {
-                        from = LocalDate.parse(args[i + 1]);
-                    } catch (DateTimeParseException e) {
-                        throw new ArgumentsParseException("Invalid value after --from");
-                    }
-                    break;
-                case "--to":
-                    try {
-                        to = LocalDate.parse(args[i + 1]);
-                    } catch (DateTimeParseException e) {
-                        throw new ArgumentsParseException("Invalid value after --to");
-                    }
-                    break;
-                case "--format":
-                    try {
-                        outputFormat = OutputFormat.valueOf(args[i + 1].toUpperCase());
-                    } catch (IllegalArgumentException e) {
-                        throw new ArgumentsParseException("Invalid value after --format");
-                    }
-                    break;
-                default:
-                    throw new ArgumentsParseException("Invalid value: value without config (--{statement})");
+            if (!pathHandler.handleArgument(args[i], settings, args[i + 1])) {
+                throw new ArgumentsParseException("Invalid value: value without config (--{statement})");
             }
             i += 2;
         }
 
-
-        if (!flagPath) {
+        if (settings.path() == null) {
             throw new ArgumentsParseException("Invalid value: path not specified");
         }
 
-        if (field == null && !value.isEmpty() || field != null && value.isEmpty()) {
+        if (settings.field() == null && !settings.value().isEmpty() || settings.field() != null
+            && settings.value().isEmpty()) {
             throw new ArgumentsParseException(
                 "Invalid value: only one of the two required values was entered: value or field");
         }
 
-        return new Config(path, from, to, outputFormat, field, value);
+        return new Config(settings.path(), settings.from(), settings.to(),
+            settings.outputFormat(), settings.field(), settings.value());
     }
 }
